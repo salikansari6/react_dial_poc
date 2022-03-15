60d0affafab78cbf6af7d597e633e76dd07f2aa1
Salik Ansari <72390270+salikansari6@users.noreply.github.com>
2022-03-15 20:17:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Dial from "./Dial";
+import "./styles.css";
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  const remoteCall = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          credit: 410
+        });
+      }, 1000);
+    });
+  };
+
+  useEffect(() => {
+    remoteCall().then((value) => {
+      setData(value);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <Dial initialAnglePosn={-135} value={data ? data.credit : 900} />
+    </div>
+  );
+}
---
+++ src/Dial.js
@@ -0,0 +1,74 @@
+import { useCallback, useEffect, useState } from "react";
+import needle from "./needle.png";
+
+const Dial = ({ value, initialAnglePosn }) => {
+  const clamp = (value, start, end) => Math.min(Math.max(value, start), end);
+
+  const [rotation, setRotation] = useState(initialAnglePosn);
+
+  const ranges = {
+    excellent: {
+      startAngle: -90,
+      endAngle: -135,
+      breakpointStart: 750,
+      breakpointEnd: 900
+    },
+    good: {
+      startAngle: 0,
+      endAngle: -90,
+      breakpointStart: 600,
+      breakpointEnd: 750
+    },
+    bad: {
+      startAngle: 90,
+      endAngle: 0,
+      breakpointStart: 0,
+      breakpointEnd: 600
+    }
+  };
+
+  const calculateRotation = useCallback(
+    (value) => {
+      let range;
+      if (value > 750) {
+        range = ranges.excellent;
+      } else if (value <= 750 && value >= 600) {
+        range = ranges.good;
+      } else {
+        range = ranges.bad;
+      }
+      const valuePercentage =
+        (value - range.breakpointStart) /
+        (range.breakpointEnd - range.breakpointStart);
+
+      const addedRotation =
+        valuePercentage * (range.endAngle - range.startAngle);
+
+      return range.startAngle + addedRotation;
+    },
+    [ranges.bad, ranges.excellent, ranges.good]
+  );
+
+  useEffect(() => {
+    const computedRotation = calculateRotation(value);
+    console.log("computed rotation " + computedRotation);
+    setRotation(computedRotation);
+  }, [value, calculateRotation]);
+
+  return (
+    <div className="dial">
+      <div className="outer-circle">
+        <div
+          className="inner-circle"
+          style={{
+            transform: `rotate(${rotation}deg)`
+          }}
+        >
+          <div className="needle" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dial;
